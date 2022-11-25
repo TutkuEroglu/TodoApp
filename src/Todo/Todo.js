86d480b4aa77b55276f2858ba20128c3a2edf820
TutkuEroglu <rrandqua@gmail.com>
2022-11-26 01:54:33 +0300

    edited todojs

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,67 +1,76 @@
-import React, { useState } from 'react'
-import "./Todo.css"
+import React, { useState } from "react";
+import "./Todo.css";
+
+let nextId = 2;
+const initialArtists = [
+  { id: 0, todo: "Cleaning", isComplete: false },
+  { id: 1, todo: "Work React", isComplete: false },
+];
 
 const Todo = () => {
-    const [list, setList] = useState([{id:1 ,todo:"Cleaning", isComplete:false}]);
-    const [todo, setTodo] = useState("");
-    const [number, setNumber] = useState(2);
+  const [list, setList] = useState(initialArtists);
+  const [todo, setTodo] = useState("");
+  
+  const AddTodo = () => {
+    setList(current => [
+      ...current,
+      { id: nextId++, todo: todo, isComplete: false },
+    ]);
+    setTodo("");
+  };
 
-    const AddTodo = () => {
-        let copyArray = [...list]
-        let copyNumber = (number)
-        copyNumber += 1
-        setNumber(copyNumber)
-        copyArray.push({
-            id: number,
-            todo:todo,
-            isComplete:false
-        })
-        setList(copyArray)
-        setTodo("")
-    }
+  const Done = (val) => {
+    setList(
+      list.map((el) =>
+        el.id === val.id ? { ...el, isComplete: !el.isComplete } : { ...el }
+      )
+    );
+  };
 
-    const Done = (val) => {
-        setList(
-            list.map(el => 
-            el.id === val.id ? {...el, isComplete: !el.isComplete} : {...el}
-            )
-        )
+  const Delete = (val) => {
+    if (val.isComplete === true) {
+      setList(list.filter((item) => item.id !== val.id));
+    } else {
+      alert("You can't delete it because you haven't completed it");
     }
-
-    const Delete = (val) => {
-        if (
-            val.isComplete === true
-        ) {
-            setList(list.filter(item => item.id !== val.id))
-        } else {
-            alert("You can't delete it because you haven't completed it")
-        }
-    }
+  };
 
   return (
     <div className="TodoApp">
-        <div className="TodoForm">
+      <div className="TodoForm">
         <span className="HeaderSpan">Todo List</span>
-        <div className="Header"> 
-            <input className="HeaderInput" onChange={(e) => setTodo(e.target.value)} value={todo}  placeholder="Todo" maxLength={9}/>
-            <button className="HeaderButton" onClick={AddTodo}>Add Todo</button>
+        <div className="Header">
+          <input
+            className="HeaderInput"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+            placeholder="Todo"
+            maxLength={9}
+          />
+          <button className="HeaderButton" onClick={AddTodo}>
+            Add Todo
+          </button>
         </div>
         <div className="Container">
-            {list.map((val) => (
-                <div className="ContainerTodos" key={val.id}>
-                <div className={val.isComplete ? "Done" : "NotDone"}>
-                    {val?.todo}
-                </div>
-                <div className="ButtonGroup"> 
-                    <button className="DoneButton" onClick={() => Done(val)}>Done</button> 
-                    <button className="DeleteButton" onClick={() => Delete(val)}>Delete</button>
-                </div>
-                </div>
-            ))}
+          {list.map((val) => (
+            <div className="ContainerTodos" key={val.id}>
+              <div className={val.isComplete ? "Done" : "NotDone"}>
+                {val?.todo}
+              </div>
+              <div className="ButtonGroup">
+                <button className="DoneButton" onClick={() => Done(val)}>
+                  Done
+                </button>
+                <button className="DeleteButton" onClick={() => Delete(val)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
